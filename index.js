a6d0dcc02ba52601ceb773bdbfe15b68489d5ac6
sk9028 <sl9028@naver.com>
2024-03-22 23:22:07 +0900

    로그인(쿠키), 인증, 로그아웃 기능 추가

--- index.js
+++ index.js
@@ -15,15 +15,24 @@ const port = 5000
 const config = require('./config/key')
 
 // 유저 모델
-const { User } = require("./models/User")
+const { User } = require('./models/User')
+
+// 인증 모델
+const { auth } = require('./middleware/auth')
 
 // 바디 파서
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
+
+// 쿠키 파서
+const cookieParser = require('cookie-parser')
 
 // application/x-www-form-urlencoded 
 // url 쿼리문자열을 자바스크립트 객체 형태로 변환하거나 객체를 url 쿼리 문자열로 변환하는 기능
 // extended : true => qs모듈(url형식 name=홍길동&pass..) false => 쿼리스트링 모듈({name : '홍길동', pass..})
 app.use(bodyParser.urlencoded({extended: true})) 
+
+// 쿠키 파서 사용
+app.use(cookieParser())
 
 // application/json
 app.use(bodyParser.json())
@@ -42,7 +51,8 @@ app.get('/', (req, res) => {
 })
 
 // '/register' => localhost:5000/register 입력하면 이게 실행됨.
-app.post('/register', async (req, res)=>{
+// 회원가입
+app.post('/api/users/register', async (req, res)=>{
   // 회원가입 전 비밀번호를 암호화 하는 작업은 User.js에 있다.(userSchema.pre함수)
   // 회원 가입 할때 필요한 정보들은 클라이언트에서 가져오면
   // 그것들을 DB에 넣어준다.
@@ -59,6 +69,71 @@ app.post('/register', async (req, res)=>{
     res.json({success : false, err})
   }) 
 })
+
+// 로그인
+app.post('/api/users/login', async (req, res) =>{
+
+  // 1. 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email : req.body.email })
+    .then(user=>{
+      if(!user){
+        return res.json({
+          loginSuccess : false,
+          message : "제공된 이메일에 해당하는 유저가 없습니다."
+        })
+      }
+
+      // 2. 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인한다.
+      user.comparePassword( req.body.password , (err, isMatch)=>{
+        if(!isMatch){
+          return res.json({loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+        }
+        // 3. 비밀번호가 맞다면 토큰을 생성한다.
+        user.generateToken((err, user)=>{
+          if(err) return res.status(400).send(err);
+
+          // 토큰을 쿠키 또는 로컬스토리지 등에 저장한다.(쿠키와 로컬스토리지는 콘솔 어플리케이션 창밑 스토리지 밑에 있음 각기 장단점이 있음. 여기서는 쿠키에 넣을 것임.)
+          // npm install cookie-parser --save
+          res.cookie("x_auth",user.token) // 이렇게하면 콘솔 쿠키에 이름이 x_auth인 유저의 쿠키값이 저장됨.
+          .status(200)
+          .json({loginSuccess : true, userId: user._id})
+
+        })
+      })
+    })  
+  .catch((err)=>{
+    return res.status(400).send(err)
+  })
+})
+
+// 인증(auth는 미들웨어)
+app.get('/api/users/auth', auth, async (req, res) =>{
+  
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true인 뜻.
+  res.status(200).json({
+    _id : req.user._id,
+    isAdmin : req.user.role === 0 ? false : true, // role 0 일반유저 , role 0이 아니면 관리자
+    isAuth : true,
+    email : req.user.email,
+    name : req.user.name,
+    lastname : req.user.lastname,
+    role : req.user.role,
+    image : req.user.image
+  })
+})
+
+// 로그아웃 기능
+app.get('/api/users/logout', auth, async (req, res) =>{
+
+  User.findOneAndUpdate({_id : req.user._id},{token : ""})
+  .then(user=>{
+    return res.status(200).send({success : true});
+  })
+  .catch((err)=>{
+    return res.status(400).send(err);
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 
 const saltRounds = 10
 
+var  jwt  =  require ( 'jsonwebtoken' ) ; 
+
 // 유저 스키마 작성
 const userSchema = mongoose.Schema({
     name: {
@@ -44,22 +46,81 @@ userSchema.pre('save', function(next){
         // saltRounds를 이용해서 비밀번호를 암호화 시킨다.
         bcrypt.genSalt(saltRounds, function(err, salt) {
             // 에러시 next로 이동
-            if(err) return next(err)
+            if(err) return next(err);
 
             bcrypt.hash(user.password, salt, function(err, hash) {
                 // 에러시 next로 이동
-                if(err) return next(err)
+                if(err) return next(err);
 
                 // 성공시 hash된 비밀번호로 치환
-                user.password = hash
+                user.password = hash;
                 
                 // 다시 원래 펑션으로 돌아간다.
-                next()
+                next();
             })
         })
+    } else{
+        next();
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, callback){
+
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return callback(err);
+        callback(null, isMatch) // true
+    })
+}
+
+userSchema.methods.generateToken = function(callback){
+
+    var user = this;
+    // jsonwebtoken을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // secretToken을 넣으면 유저 아이디가 나온다는 뜻
+    //user.id + 'secretToken' = token
+
+    user.token = token
+    
+    // 구버전은 주석된 부분과 같이 작성해야 함.
+    // user.save(function(err, user){
+    //     if(err) return callback(err)
+    //     callback(null, user)
+    // })
+
+    // 이게 신버전
+    user.save()
+    .then(user=>{
+        return callback(null, user)
+    }) 
+    .catch((err)=>{
+        return callback(err)
+    })
+}
+
+userSchema.statics.findByToken = function(token, callback){
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인한다.
+        user.findOne({"_id" : decoded, "token" : token })
+        // (구)버전 소스
+        //if(err) return callback(err);
+        //callback(null, user)
+
+        // (신)버전 소스
+        .then(user=>{
+            callback(null, user)
+        })  
+        .catch((err)=>{
+            return callback(err);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
 module.exports = {User}
+
